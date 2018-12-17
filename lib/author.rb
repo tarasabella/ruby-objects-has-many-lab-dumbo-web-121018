b9a7421627a4a7ed3ea5528f7b7f7f4265361122
Tara Sabella <tarasabella@gmail.com>
2018-12-17 17:15:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,11 @@
 class Author 
 
 attr_reader :name
-#Author has a name
+# Author has a name
 
-#an author has many posts 
-    #set the instance variable posts equal to an empty array
-    #to store songs
+# an author has many posts 
+#     set the instance variable posts equal to an empty array
+#     to store songs
   def initialize (name)
     @name = name 
     #an author has a name
