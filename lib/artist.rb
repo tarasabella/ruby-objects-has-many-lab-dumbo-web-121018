2995edab25867c8e5dfdfafc6837d1342ab9ea90
Tara Sabella <tarasabella@gmail.com>
2018-12-17 17:17:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,35 +5,35 @@ class Artist
 
 
   def initialize (name)
-    @name = name #an artist has a name
-    #an artist has songs it should know
-    #set the instance variable songs equal to an empty array
-    #to store songs
-  end
-  def songs 
-    Song.all.select do |song|
-      self == song.artist
-    end 
+#     @name = name #an artist has a name
+#     #an artist has songs it should know
+#     #set the instance variable songs equal to an empty array
+#     #to store songs
+#   end
+#   def songs 
+#     Song.all.select do |song|
+#       self == song.artist
+#     end 
 
-  end 
+#   end 
   
-end 
-#an Artist should know the name of their songs
+# end 
+# #an Artist should know the name of their songs
 
-# def self.all
-#   @@all
-# end 
-# end 
+# # def self.all
+# #   @@all
+# # end 
+# # end 
   
-# def songs 
-#   @songs 
-# end 
+# # def songs 
+# #   @songs 
+# # end 
 
-# # def add_song(song)
-#   Artist.all.select do |title|
-#   title.name == self 
-# end 
+# # # def add_song(song)
+# #   Artist.all.select do |title|
+# #   title.name == self 
+# # end 
 
-# def add_song_by
+# # def add_song_by
 
-# end 
+# # end 
