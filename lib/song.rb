9a654ead172346fe4152d5adbad4b52096c03a38
Tara Sabella <tarasabella@gmail.com>
2018-12-17 17:21:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,9 @@ def initialize(name, artist)
   @name = name 
   @artist = artist 
  #each song must have a name and belong to an artist 
+ 
+ @@all << self 
+ #Each time a song instance is created its shoeveled to teh song array 
   end 
 end 
 
